5d0298b14b4e1782e81dba6b0b078158c9d5438f
SenudaK <senudad@gmail.com>
2025-11-28 13:54:57 +0530

    Remove node_modules and add .gitignore

---
+++ backend/src/test/java/com/nsbm_projects/hotel_management_system/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm_projects.hotel_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
